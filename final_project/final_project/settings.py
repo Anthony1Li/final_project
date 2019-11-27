a9b1fd872b1a092b00052a4367d754182884cf67
jiyao Li <jiyaoli95@gmail.com>
2019-11-27 01:27:05 -0500

    commit6 sightings was paginated

--- final_project/final_project/settings.py
+++ final_project/final_project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp.apps.MyappConfig'
+    'myapp.apps.MyappConfig',
+    #'myapp'
+    'leaflet'
 ]
 
 MIDDLEWARE = [
