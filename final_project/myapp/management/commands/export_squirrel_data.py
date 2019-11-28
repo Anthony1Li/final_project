31012a9c63d3822d4f480dc71f4aa821c1611bbb
jiyao Li <jiyaoli95@gmail.com>
2019-11-28 00:46:22 -0500

    wrote a export function

---
+++ final_project/myapp/management/commands/export_squirrel_data.py
@@ -0,0 +1,19 @@
+from django.core.management import BaseCommand
+from myapp.models import squirrel_data
+import csv
+import sys
+
+class Command(BaseCommand):
+    help = 'import data'
+
+    def add_arguments(self, parser):
+        parser.add_argument('path',type=str,help="csv file")
+
+    def handle(self, path, **options):
+        with open(path, 'w', newline='') as f:
+            model = squirrel_data
+            field_names = [fa.name for fa in model._meta.fields]
+            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+            writer.writerow(field_names)
+            for instance in model.objects.all():
+                writer.writerow([getattr(instance, fi) for fi in field_names])
